3acfef9815aa46843b28ffad6c20c979372fa57b
Graham Hay <grahamrhay@gmail.com>
2015-12-31 12:11:20 +0000

    Load presents

---
+++ src/sc_loading_elf.erl
@@ -0,0 +1,41 @@
+-module(sc_loading_elf).
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    lager:info("Started loading presents"),
+    gen_server:cast(self(), load_present),
+    {ok, #{}}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ignored, State}.
+
+handle_cast(load_present, State) ->
+    ok = gen_server:call(sc_load_q, get, infinity),
+    sc_present:load(),
+    ok = gen_server:call(sc_deliver_q, add, infinity),
+    gen_server:cast(self(), load_present),
+    {noreply, State};
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
--- src/sc_present.erl
+++ src/sc_present.erl
@@ -2,6 +2,7 @@
 
 -export([make/0]).
 -export([wrap/0]).
+-export([load/0]).
 
 make() ->
     lager:info("Making present"),
@@ -10,3 +11,7 @@ make() ->
 wrap() ->
     lager:info("Wrapping present"),
     timer:sleep(10).
+
+load() ->
+    lager:info("Loading present"),
+    timer:sleep(5).
